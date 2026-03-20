f4b8e9a674395115c11aafb1d635e7978a49af94
NoamRobertsson <noamrobertsson03@outlook.com>
2026-03-20 22:03:49 +0100

    Minorgem item class that implements ISellable

---
+++ OBP200-RolePlayingGame/Refactor/MinorGem.cs
@@ -0,0 +1,13 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public class MinorGem : Item, ISellable
+{
+    private const int SellValue = 5;
+    
+    public MinorGem(string name) : base(name){ }
+
+    public int GetSellPrice()
+    {
+        return SellValue;
+    }
+}
