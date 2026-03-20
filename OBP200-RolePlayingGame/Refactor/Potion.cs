66cc53ea3d658a5f75778dbba203f7d883371d13
NoamRobertsson <noamrobertsson03@outlook.com>
2026-03-20 22:04:09 +0100

    Implemented potion item that implements IUsable

---
+++ OBP200-RolePlayingGame/Refactor/Potion.cs
@@ -0,0 +1,13 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public class Potion : Item, IUsable
+{
+    private const int HealAmount = 12;
+    
+    public Potion(string name) : base(name){ }
+
+    public void Use(Player player)
+    {
+        player.Heal(HealAmount);
+    }
+}
