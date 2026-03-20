ef9ac6d16e60dcea34144c6a338f7ff20b51e1ad
NoamRobertsson <noamrobertsson03@outlook.com>
2026-03-20 22:09:13 +0100

    Feat: Add and remove functionality for items in inventory

--- OBP200-RolePlayingGame/Refactor/Inventory.cs
+++ OBP200-RolePlayingGame/Refactor/Inventory.cs
@@ -2,5 +2,15 @@
 
 public class Inventory
 {
-    private List<Item> _items = [];
+    private List<Item> _items = new();
+
+    public void Add(Item item)
+    {
+        _items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        _items.Remove(item);
+    }
 }
