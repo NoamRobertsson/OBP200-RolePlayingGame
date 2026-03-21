e23c82ea6eac3a30f409c2dea724d9a98aad490a
NoamRobertsson <noamrobertsson03@outlook.com>
2026-03-21 19:02:50 +0100

    Added abstract ClassType that can be accessed in class-related methods

---
+++ OBP200-RolePlayingGame/Refactor/ClassType.cs
@@ -0,0 +1,9 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public abstract class ClassType
+{
+    public abstract (int Maxhp, int Atk, int Def) LevelUpStats { get; protected set; }
+    
+    public abstract int DmgBuff(); // Konstant buff för alla klasser förutom rogue   
+    public abstract int SpecialAttackDamage(int enemyDef, bool vsBoss, Player player);
+}
