2bce1d72a1370b325311a468ad5ab06d845635e3
NoamRobertsson <noamrobertsson03@outlook.com>
2026-03-23 12:53:48 +0100

    Re: IClassType that hold class specific methods and stats

---
+++ OBP200-RolePlayingGame/Refactor/IClassType.cs
@@ -0,0 +1,10 @@
+﻿namespace OBP200_RolePlayingGame.Refactor;
+
+public interface IClassType
+{
+    string Name { get;}
+    double RunAwayChance { get; } 
+    void ApplyLevelUp(Player player);
+    int DmgBuff(Random rng); // Konstant buff för alla klasser förutom rogue   
+    int SpecialAttackDamage(int enemyDef,Random rng, Player player);
+}
