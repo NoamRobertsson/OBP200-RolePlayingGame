ced740817cd9a6cd808fd7b9c4940f15ac99c05c
NoamRobertsson <noamrobertsson03@outlook.com>
2026-03-23 13:00:02 +0100

    Chore: Added SpecialAttackContext to transfer info between class and player

---
+++ OBP200-RolePlayingGame/Refactor/PlayerClassContext.cs
@@ -0,0 +1,12 @@
+﻿namespace OBP200_RolePlayingGame.Refactor;
+
+// Kontext klass som skickas till IClassType för att undvika att hela player viass
+public class SpecialAttackContext
+{
+    public int PlayerAtk { get; init; }
+    public int enemyDef { get; init; }
+    public Random rng { get; init; } = default!;  
+    
+    public Func<int, bool> TrySpendGold { get; init; } // För mage
+    public Action<int> TakeDamage { get; init; } // För warrior
+}
