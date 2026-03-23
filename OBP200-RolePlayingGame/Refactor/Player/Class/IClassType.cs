d47cd5ead2f4e28dedaecaf016763d51f838dd1a
NoamRobertsson <noamrobertsson03@outlook.com>
2026-03-23 17:29:43 +0100

    re: removed static from name and basestats

--- OBP200-RolePlayingGame/Refactor/Player/Class/IClassType.cs
+++ OBP200-RolePlayingGame/Refactor/Player/Class/IClassType.cs
@@ -2,9 +2,9 @@
 
 public interface IClassType
 {
-    static string Name { get;}
+    string Name { get;}
     double RunAwayChance { get; } 
-    static (int maxHp, int  hp, int atk, int def, int potions, int gold) BaseStats { get; }
+    (int maxHp, int  hp, int atk, int def, int potions, int gold) BaseStats { get; }
     (int MaxHp, int Atk, int Def) LevelUpStats { get; }
     int DmgBuff(Random rng); // Konstant buff för alla klasser förutom rogue   
     int SpecialAttackDamage(SpecialAttackContext context);
